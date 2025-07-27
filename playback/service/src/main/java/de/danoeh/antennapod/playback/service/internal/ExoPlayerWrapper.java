7a321df144ae3e4860bf5b4bf02a786e1b9d861b
Michael Edoror <medoror@gmail.com>
2025-07-27 18:28:57 -0500

    Whitespace

--- playback/service/src/main/java/de/danoeh/antennapod/playback/service/internal/ExoPlayerWrapper.java
+++ playback/service/src/main/java/de/danoeh/antennapod/playback/service/internal/ExoPlayerWrapper.java
@@ -272,7 +272,7 @@ public class ExoPlayerWrapper {
 
     public void setPlaybackParams(float speed, boolean skipSilence) {
         playbackParameters = new PlaybackParameters(speed, playbackParameters.pitch);
-        
+
         // Fix for issue #7203: Ensure proper ordering to prevent audio distortion
         // When both speed changes and skip silence are enabled, we need to:
         // 1. Set playback parameters first to configure speed processing
